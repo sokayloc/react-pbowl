0428795c9ad512b908cf673b6c0e2905e2dbbb31
Makalo Chares <sokayloc@skiff.com>
2023-09-12 14:52:18 -0400

    stuff

---
+++ src/Nav.jsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+      return(
+            <div>
+                  <h6>Home</h6>
+                  <h6>Players</h6>
+            </div>
+      )
+}
+
+export default Nav
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+      return(
+            <div>
+                  <h1>HomePage</h1>
+            </div>
+      )
+}
+
+export default Homepage
